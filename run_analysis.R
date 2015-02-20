0a22068b66c3329b96f289d47ecdc4bf46198175
Vaughan Dutton <vaughan.dutton@stx.oxon.org>
2015-02-20 16:17:28 +0000

    Getting and cleaning data

---
+++ run_analysis.R
@@ -0,0 +1,31 @@
+install.packages("reshape2")
+install.packages("data.table")
+library(reshape2)
+library(data.table)
+activity_labels <- read.table("activity_labels.txt")[,2]
+features <- read.table("features.txt")[,2]
+extract_features <- grepl("mean|std", features)
+X_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
+names(X_test) = features
+X_test = X_test[,extract_features]
+y_test[,2] = activity_labels[y_test[,1]]
+names(y_test) = c("Activity_ID", "Activity_Label")
+names(subject_test) = "subject"
+test_data <- cbind(as.data.table(subject_test), y_test, X_test)
+X_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
+names(X_train) = features
+X_train = X_train[,extract_features]
+y_train[,2] = activity_labels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+names(subject_train) = "subject"
+train_data <- cbind(as.data.table(subject_train), y_train, X_train)
+data = rbind(test_data, train_data)
+id_labels = c("subject", "Activity_ID", "Activity_Label")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data = melt(data, id = id_labels, measure.vars = data_labels)
+tidy_data = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+write.table(tidy_data, file = "./tidy_data.txt", row.names=FALSE)
